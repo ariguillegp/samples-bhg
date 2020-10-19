f7bcbafdbe25224831af2850ddf971b4ea21b038
Aristides Gonzalez <apol@conquestcyber.com>
2020-10-18 21:25:42 -0400

    added shodan host search

--- shodan/cmd/client/main.go
+++ shodan/cmd/client/main.go
@@ -9,6 +9,9 @@ import (
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		log.Fatalln("Usage: shodan searchterm")
+	}
 	apiKey := os.Getenv("SHODAN_API_KEY")
 	s := shodan.New(apiKey)
 	info, err := s.APIInfo()
@@ -20,4 +23,13 @@ func main() {
 		"Query Credits: %d\nScan Credits: %d\n\n",
 		info.QueryCredits,
 		info.ScanCredits)
+
+	hostSearch, err := s.HostSearch(os.Args[1])
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	for _, host := range hostSearch.Matches {
+		fmt.Printf("%18s%8d\n", host.IPString, host.Port)
+	}
 }
--- shodan/shodan/shodan.go
+++ shodan/shodan/shodan.go
@@ -19,8 +19,7 @@ func New(apiKey string) *Client {
 }
 
 func (s *Client) APIInfo() (*APIInfo, error) {
-	reqURL := fmt.Sprintf("%s/api-info?key=%s", BaseURL, s.apiKey)
-	res, err := http.Get(reqURL)
+	res, err := http.Get(fmt.Sprintf("%s/api-info?key=%s", BaseURL, s.apiKey))
 	if err != nil {
 		return nil, err
 	}
@@ -33,6 +32,16 @@ func (s *Client) APIInfo() (*APIInfo, error) {
 	return &ret, nil
 }
 
-func (s *Client) HostSearch() {
-	return
+func (s *Client) HostSearch(q string) (*HostSearch, error) {
+	res, err := http.Get(fmt.Sprintf("%s/shodan/host/search?key=%s&query%s", BaseURL, s.apiKey, q))
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	var ret HostSearch
+	if err := json.NewDecoder(res.Body).Decode(&ret); err != nil {
+		return nil, err
+	}
+	return &ret, nil
 }
